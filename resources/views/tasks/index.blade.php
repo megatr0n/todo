b38f4b27b0bbaca1f60af6e7375f30aab30dea10
megatr0n <abehinds@msn.com>
2023-07-07 18:43:48 -0500

    drag&drop and database update working.
    Signed-off-by: megatr0n <abehinds@msn.com>

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -49,24 +49,26 @@
                                 <th>Task</th>
                                 <th>&nbsp;</th>
                             </thead>
-                            <tbody>
-                                @foreach ($tasks as $task)
-                                    <tr>
-                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+                            <tbody  class="list-group  connectedSortable" id="complete-item-drop">
+								@if(!empty($completeItem) && $completeItem->count())
+									@foreach ($completeItem as $key => $task)
+										<tr item-id="{{ $task->id }}">
+											<td class="table-text"><div>{{ $task->name }}</div></td>
 
-                                        <!-- Task Delete Button -->
-                                        <td>
-                                            <form action="{{url('task/' . $task->id)}}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
+											<!-- Task Delete Button -->
+											<td>
+												<form action="{{url('task/' . $task->id)}}" method="POST">
+													{{ csrf_field() }}
+													{{ method_field('DELETE') }}
 
-                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+													<button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+														<i class="fa fa-btn fa-trash"></i>Delete
+													</button>
+												</form>
+											</td>
+										</tr>
+									@endforeach
+								@endif	
                             </tbody>
                         </table>
                     </div>
@@ -74,4 +76,46 @@
             @endif
         </div>
     </div>	
+
+				
+	<!-- scripts for drag&drop below -->
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script>
+	  $( function() {
+		$( "#padding-item-drop, #complete-item-drop" ).sortable({
+		  connectWith: ".connectedSortable",
+		  opacity: 0.5,
+		});
+		$( ".connectedSortable" ).on( "sortupdate", function( event, ui ) {
+			var pending = [];
+			var accept = [];
+			$("#padding-item-drop li").each(function( index ) {
+			  if($(this).attr('item-id')){
+				pending[index] = $(this).attr('item-id');
+			  }
+			});
+			$("#complete-item-drop li, div, tr").each(function( index ) {
+			  accept[index] = $(this).attr('item-id');
+			});
+			$.ajax({
+				url: "{{ route('update.items') }}",
+				method: 'POST',
+				headers: {
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				},
+				data: {pending:pending,accept:accept},
+				success: function(data) {
+				  console.log('success');
+				}
+			});
+			  
+		});
+	  });
+	</script>	
+	<!-- scripts for drag&drop above -->			
+	
+
+	
 @endsection
